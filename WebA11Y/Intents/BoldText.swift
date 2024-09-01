499e5f859aa5576d4b4eb1804d954abcb5c3da45
Cizzuk <3@cizzuk.net>
2024-09-01 09:27:03 +0900

    Enum -> intent

--- WebA11Y/Intents/BoldText.swift
+++ WebA11Y/Intents/BoldText.swift
@@ -9,14 +9,26 @@ import Foundation
 import AppIntents
 
 @available(iOS 16.0, macOS 13.0, watchOS 9.0, tvOS 16.0, *)
+enum BoldTextEnum: String, AppEnum {
+    case turn
+    case toggle
+
+    static var typeDisplayRepresentation = TypeDisplayRepresentation(name: "Toggle")
+    static var caseDisplayRepresentations: [Self: DisplayRepresentation] = [
+        .turn: "Turn",
+        .toggle: "Toggle"
+    ]
+}
+
+@available(iOS 16.0, macOS 13.0, watchOS 9.0, tvOS 16.0, *)
 struct BoldText: AppIntent, CustomIntentMigratedAppIntent, PredictableIntent {
     static let intentClassName = "BoldTextIntent"
 
-    static var title: LocalizedStringResource = "Bold Text on the Web"
-    static var description = IntentDescription("Change Bold Text setting")
+    static var title: LocalizedStringResource = "Set Bold Text on the Web"
+    static var description = IntentDescription("Sets the Bold Text to on or off on the Web.")
 
     @Parameter(title: "Toggle", default: .turn)
-    var toggle: EnumAppEnum?
+    var toggle: BoldTextEnum?
 
     @Parameter(title: "Setting", default: false)
     var setting: Bool?
@@ -32,28 +44,19 @@ struct BoldText: AppIntent, CustomIntentMigratedAppIntent, PredictableIntent {
     static var predictionConfiguration: some IntentPredictionConfiguration {
         IntentPrediction(parameters: (\.$setting, \.$toggle)) { setting, toggle in
             DisplayRepresentation(
-                title: "\(toggle!) Bold Text \(setting! ? "On":"Off") on the Web",
-                subtitle: ""
+                title: "\(toggle!) Bold Text \(setting! ? "On":"Off") on the Web"
             )
         }
         IntentPrediction(parameters: (\.$toggle)) { toggle in
             DisplayRepresentation(
-                title: "\(toggle!) Bold Text on the Web",
-                subtitle: ""
-            )
-        }
-        IntentPrediction(parameters: (\.$setting)) { setting in
-            DisplayRepresentation(
-                title: "Turn Bold Text ",
-                subtitle: ""
+                title: "\(toggle!) Bold Text on the Web"
             )
         }
     }
-
+    
     func perform() async throws -> some IntentResult {
         // TODO: Place your refactored intent handler code here.
         let userDefaults = UserDefaults(suiteName: "group.com.tsg0o0.safariweba11y")
-        let boldText = UserDefaults(suiteName: "group.com.tsg0o0.safariweba11y")!.bool(forKey: "boldText")
         
         if toggle == .turn {
             if setting == true {
@@ -62,6 +65,7 @@ struct BoldText: AppIntent, CustomIntentMigratedAppIntent, PredictableIntent {
                 userDefaults!.set("false", forKey: "boldText")
             }
         } else if toggle == .toggle {
+            let boldText = UserDefaults(suiteName: "group.com.tsg0o0.safariweba11y")!.bool(forKey: "boldText")
             if boldText == false {
                 userDefaults!.set("true", forKey: "boldText")
             } else {
@@ -70,25 +74,6 @@ struct BoldText: AppIntent, CustomIntentMigratedAppIntent, PredictableIntent {
         }
         userDefaults!.synchronize()
         return .result()
-    }
-}
-
-@available(iOS 16.0, macOS 13.0, watchOS 9.0, tvOS 16.0, *)
-fileprivate extension IntentDialog {
-    static func toggleParameterDisambiguationIntro(count: Int, toggle: EnumAppEnum) -> Self {
-        "There are \(count) options matching ‘\(toggle)’."
-    }
-    static func toggleParameterConfirmation(toggle: EnumAppEnum) -> Self {
-        "Just to confirm, you wanted ‘\(toggle)’?"
-    }
-    static var settingParameterPrompt: Self {
-        "Would you like to turn the setting on or off?"
-    }
-    static func settingParameterDisambiguationIntro(count: Int, setting: Bool) -> Self {
-        "There are \(count) options matching ‘\(setting ? "On":"Off")’."
-    }
-    static func settingParameterConfirmation(setting: Bool) -> Self {
-        "Just to confirm, you wanted ‘\(setting ? "On":"Off")’?"
     }
 }
 
