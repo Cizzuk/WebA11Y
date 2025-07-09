630a376ce20857738dfe028f0e6d5e021e40ed0c
Cizzuk <dropcat@cizzuk.net>
2025-07-09 22:58:21 +0900

    Update Extension Scripts

--- WebA11Y/WebA11Y Extension/Resources/background.js
+++ WebA11Y/WebA11Y Extension/Resources/background.js
@@ -1,10 +1,27 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.type == "content") {
-        browser.runtime.sendNativeMessage("com.tsg0o0.safariweba11y.Extension", function(response) {
-            const Data = JSON.parse(response);
-            sendResponse(Data);
+'use strict';
+
+browser.runtime.onMessage.addListener((request, sender) => { insertCSS(sender.tab.id); });
+browser.tabs.onActivated.addListener((tabData) => { insertCSS(tabData.tabId); });
+
+function insertCSS(tabId) {
+    browser.runtime.sendNativeMessage("com.tsg0o0.safariweba11y.Extension", function(response) {
+        const resData = JSON.parse(response);
+        if (resData.type != "run" || !resData.style) {
+            console.log("[WebA11Y] Canceled");
             return;
+        }
+        
+        console.log("[WebA11Y] Trying to insert CSS:", resData.style);
+        browser.scripting.insertCSS({
+            target: {
+                tabId: tabId,
+            },
+            origin: "author",
+            css: resData.style,
+        }).catch((err) => {
+            console.error("[WebA11Y] Error inserting CSS:", err);
         });
-    }
-    return true;
-});
+        
+        return;
+    });
+}
--- WebA11Y/WebA11Y Extension/Resources/content.js
+++ WebA11Y/WebA11Y Extension/Resources/content.js
@@ -1,29 +1,5 @@
-let _safariweba11y_data = {};
-browser.runtime.sendMessage({ type: "content" }, function(response) {
-    if (response.type != "run" || !response.style) {
-        return;
-    }
-    
-    _safariweba11y_data["style"] = response.style;
-    _safariweba11y_complete("response");
-});
+'use strict';
 
 document.addEventListener('DOMContentLoaded', function() {
-    _safariweba11y_complete("interactive");
+    browser.runtime.sendMessage({});
 });
-
-function _safariweba11y_complete(flag) {
-    _safariweba11y_data[flag] = true;
-    if (_safariweba11y_data["response"] && _safariweba11y_data["interactive"] && !_safariweba11y_data["done"]) {
-        let customStyle = _safariweba11y_data["style"];
-        try {
-            let styleSheet = document.styleSheets.item(0);
-            styleSheet.insertRule(customStyle, styleSheet.cssRules.length);
-        } catch {
-            let styleElement = document.createElement("style")
-            styleElement.appendChild(document.createTextNode(customStyle));
-            document.body.appendChild(styleElement);
-        }
-        _safariweba11y_data["done"] = true;
-    }
-}
